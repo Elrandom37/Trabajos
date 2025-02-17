2b51ce7aedb9fb64c2a75ab3ba36ab2419a1147b
Elrandom37 <ailen.vanegas@gmail.com>
2025-02-17 07:29:01 -0500

    Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,5 +1,4 @@
 
-# Definir las funciones para cada operación
 def suma(num1, num2):
     return num1 + num2
 
@@ -15,21 +14,17 @@ def division(num1, num2):
     else:
         return num1 / num2
 
-# Mostrar las opciones al usuario
 print("Calculadora simple")
 print("1. Suma")
 print("2. Resta")
 print("3. Multiplicación")
 print("4. División")
 
-# Pedir al usuario que elija una opción
 opcion = int(input("Elija una opción (1/2/3/4): "))
 
-# Pedir al usuario que ingrese los números
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
-# Realizar la operación seleccionada por el usuario
 if opcion == 1:
     resultado = suma(num1, num2)
     print("El resultado de la suma es:", resultado)
