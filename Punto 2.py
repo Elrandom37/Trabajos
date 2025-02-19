0c9dbba746fd002c0773ed41bc1a0a3213a745dc
Elrandom37 <ailen.vanegas@gmail.com>
2025-02-19 07:44:27 -0500

    Add files via upload

---
+++ Punto 2.py
@@ -0,0 +1,8 @@
+def calcular_precio_final():
+    precio_original = float(input("Introduce el precio original del artículo: "))
+    descuento = 0.15
+    precio_final = precio_original * (1 - descuento)
+    print(f"El precio final después del descuento es: {precio_final:.2f}")
+
+# Llamada a la función
+calcular_precio_final()
