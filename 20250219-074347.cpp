c0e9b2f1682c73909a17b12f21efbc5c41e2ed3f
Elrandom37 <ailen.vanegas@gmail.com>
2025-02-19 07:44:40 -0500

    Add files via upload

---
+++ 20250219-074347.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double precioOriginal;
+    double descuento = 0.15; // 15% de descuento
+
+    cout << "Ingrese el precio del artículo: ";
+    cin >> precioOriginal;
+
+    double precioFinal = precioOriginal * (1 - descuento);
+    cout << "El precio final después del descuento es: " << precioFinal << endl;
+
+    return 0;
+}
